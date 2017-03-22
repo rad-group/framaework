02f34d1a5bb82103f3144e7a1b749781be4a8758
Matteo Chiti <info@matteochiti.it>
2017-03-22 17:57:11 +0100

    Add icons to iconae. Add Off Canvas.

---
+++ js/dev/global/off-canvas.js
@@ -0,0 +1,41 @@
+;(function ($) {
+
+  // Document Ready
+  $( document ).ready(function() {
+
+    open_off_canvas();
+    close_off_canvas();
+
+  }); //END Document Ready
+
+  function open_off_canvas() {
+    var $btn = $('.js--off-canvas-action');
+
+    $btn.click(function(){
+      var target = $(this).data( "off-canvas" );
+
+      $('#' + target).addClass('opened');
+
+      $('body').addClass('off-canvas-opened');
+    })
+  }
+
+  function close_off_canvas() {
+
+    function remove_off_canvas() {
+      $('.js--off-canvas').removeClass('opened');
+      $('body').removeClass('off-canvas-opened');
+    }
+
+    $(document).keyup(function(e) {
+      if (e.keyCode == 27) {
+        remove_off_canvas();
+      }
+    });
+
+    $('.js--close-popup').click(function(){
+      remove_off_canvas();
+    })
+  }
+
+}(jQuery));
