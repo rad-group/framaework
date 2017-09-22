9e19f78b61c5f52cafbf1b421e604fc8f617462d
Matteo Chiti <info@matteochiti.it>
2017-09-22 12:41:30 +0200

    Accordions don't close when content is clicked.

--- js/dev/global/accordion.js
+++ js/dev/global/accordion.js
@@ -9,22 +9,24 @@
 
   function accordion() {
 
-    var $single_accordion = $('.js--accordion__single');
+    var $single_accordion_header = $('.js--accordion__single-header');
+    var $single_accordion = $single_accordion_header.parent();
     var $single_accordion_content = $('.js--accordion__single-content');
 
 
-    $single_accordion.click(function() {
+    $single_accordion_header.click(function() {
 
-      var $this_single_accordion_content = $(this).find('.js--accordion__single-content');
+      var $this_single_accordion_content = $(this).parent().find('.js--accordion__single-content');
+      var $this_single_accordion = $(this).parent();
 
-      if ( $(this).hasClass('opened') ) {
-        $(this).removeClass('opened');
+      if ( $this_single_accordion.hasClass('opened') ) {
+        $this_single_accordion.removeClass('opened');
         $this_single_accordion_content.slideUp();
       } else {
         $single_accordion.removeClass('opened');
         $single_accordion_content.slideUp();
 
-        $(this).addClass('opened');
+        $this_single_accordion.addClass('opened');
         $this_single_accordion_content.slideDown();
       }
 
