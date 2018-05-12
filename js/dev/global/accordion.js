6d3a1037577940e7f0f900afea06199880048f2e
RAD - Matteo Chiti <matteo@we-rad.com>
2018-05-12 22:32:25 +0200

    Buttons upgrade: add outline style. Minor Bug Fixes.

--- js/dev/global/accordion.js
+++ js/dev/global/accordion.js
@@ -7,27 +7,29 @@ $( document ).ready(function() {
 
 function accordion() {
 
-  var $single_accordion_header = $('.js--accordion__single-header');
-  var $single_accordion = $single_accordion_header.parent();
-  var $single_accordion_content = $('.js--accordion__single-content');
+  $('.js--accordion').each(function(){
+    var $single_accordion_header = $(this).find('.js--accordion__single-header');
+    var $single_accordion = $single_accordion_header.parent();
+    var $single_accordion_content = $(this).find('.js--accordion__single-content');
 
 
-  $single_accordion_header.click(function() {
+    $single_accordion_header.click(function() {
 
-    var $this_single_accordion_content = $(this).parent().find('.js--accordion__single-content');
-    var $this_single_accordion = $(this).parent();
+      var $this_single_accordion_content = $(this).parent().find('.js--accordion__single-content');
+      var $this_single_accordion = $(this).parent();
 
-    if ( $this_single_accordion.hasClass('opened') ) {
-      $this_single_accordion.removeClass('opened');
-      $this_single_accordion_content.slideUp();
-    } else {
-      $single_accordion.removeClass('opened');
-      $single_accordion_content.slideUp();
+      if ( $this_single_accordion.hasClass('opened') ) {
+        $this_single_accordion.removeClass('opened');
+        $this_single_accordion_content.slideUp();
+      } else {
+        $single_accordion.removeClass('opened');
+        $single_accordion_content.slideUp();
 
-      $this_single_accordion.addClass('opened');
-      $this_single_accordion_content.slideDown();
-    }
+        $this_single_accordion.addClass('opened');
+        $this_single_accordion_content.slideDown();
+      }
+
+    });
 
   });
-
 }
